7df88b95b76e412b663b0a6aae31cbd1c569c236
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:17:48 +0700

    latihan data 1a

--- src/main/kotlin/Latihan.kt
+++ src/main/kotlin/Latihan.kt
@@ -1,13 +1,26 @@
-fun main () {
+//pertemuan 1
+//fun main () {
+//
+//    println ("Hello friend!")
+//    println ("Lauraaa")
+//
+//    print("Pekerjaan : ")
+//    val pekerjaan = readLine()
+//
+//    print("Alamat : ")
+//    val alamat = readLine()
+//
+//    println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+//}
 
-    println ("Hello friend!")
-    println ("Lauryeah")
+//pertemuan 2
+fun main (args: Array<String>){
+    var namaLengkap: String = "Laura Ladiesga"
+    var alamat: String
+    alamat = "Yogyakarta"
+    var umur = 19
 
-    print("Pekerjaan : ")
-    val pekerjaan = readLine()
-
-    print("Alamat : ")
-    val alamat = readLine()
-
-    println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
 }
